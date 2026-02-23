d2be5295d67fe30220e696fd3a8db215f7ed005d
Chase Donahoe <chase@Chases-MBP.lan>
2026-02-23 12:26:55 -0800

    Fix server.js - use express.static instead of serve-static

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const path = require('path');
-const serveStatic = require('serve-static');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 // Serve static files from the React app build directory
-app.use(serveStatic(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 // Catch all handler: send back React's index.html file for any non-API routes
 app.get('*', (req, res) => {
